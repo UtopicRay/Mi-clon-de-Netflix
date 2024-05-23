88ac5d726fa7087b4c992d7103d228a902707b5f
UtopicRay <ray2021@gmail.com>
2024-05-23 00:11:48 -0400

    'AuthContext Creado'

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,15 @@
+import { useState} from "react";
+import {auth} from "../../firebase.js";
+import {AuthContext} from "./AuthContext.js";
+
+export const AuthProvider = ({children}) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    auth.onAuthStateChanged((user) => {
+        setCurrentUser(user.email);
+    })
+    return (
+        <AuthContext.Provider value={{currentUser}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
